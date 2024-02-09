3f11a6a1d23aa39b017beea4de9802cf58469942
Ashu11-A <Matheusn.biolowons@gmail.com>
2024-02-09 14:35:29 -0400

    🧱 Release build

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,40 @@
 import { createClient } from './discord/base'
-import { log } from './settings'
+import { log, processEnv } from './settings'
 import Loggings from '@/controllers/Loggings'
-import { dirCR } from '@/functions'
+import { dirCR, dirEX, jsonsv } from '@/functions'
 import { QuickDB } from 'quick.db'
 import { join } from 'path'
-
+import { writeFileSync } from 'fs'
+import axios from 'axios'
 export * from 'colors'
-const core = new Loggings('All', 'blue')
 
 const client = createClient()
-client.start()
-
-process.on('uncaughtException', log.error)
-process.on('unhandledRejection', log.error)
-
+const core = new Loggings('All', 'blue')
 const rootDir = process.cwd()
 
 dirCR(`${rootDir}/database`)
+
+async function checkConfig (): Promise<void> {
+  if (processEnv.BOT_TOKEN === undefined) {
+  // Generate .env content
+    const envContent = Object.entries({ BOT_TOKEN: 'MeTroque' })
+      .map(([key, value]) => `${key}=${value}`)
+      .join('\n')
+    // Write .env file
+    writeFileSync('.env', envContent)
+  }
+  if (processEnv.BOT_TOKEN === 'MeTroque') {
+    throw new Error('Defina o token em .env', { cause: 'Token não fornecido' })
+  }
+
+  if (dirEX('./settings/settings.json')) {
+    await axios.get('https://raw.githubusercontent.com/Ashu11-A/PaymentBot/main/src/settings/settings.exemple.json')
+      .then((res) => {
+        dirCR(`${rootDir}/settings`)
+        jsonsv('./settings/settings.json', res.data)
+      })
+  }
+}
 
 const db = {
   guilds: new QuickDB({ filePath: join(rootDir, 'database/guilds.sqlite'), table: 'guilds' }),
@@ -30,5 +48,11 @@ const db = {
   ctrlPanel: new QuickDB({ filePath: join(rootDir, 'database/ctrlPanel.sqlite') }),
   pterodactyl: new QuickDB({ filePath: join(rootDir, 'database/pterodactyl.sqlite') })
 }
+
+void checkConfig().then(() => {
+  client.start()
+  process.on('uncaughtException', log.error)
+  process.on('unhandledRejection', log.error)
+})
 
 export { client, core, db }
