297c3f27c5bb54e5015a14bdf75c8dbd07168789
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-04-21 03:06:06 -0400

    ✨ Refactor: remove if, implement switch

--- src/discord/components/controller.ts
+++ src/discord/components/controller.ts
@@ -1,8 +1,7 @@
 import { EmbedBuilder, type AnySelectMenuInteraction, type ButtonInteraction, type CacheType, type ModalSubmitInteraction } from 'discord.js'
-import { collectorEditButtons } from './SUEE/collectorButtons'
-import { collectorEditModal } from './SUEE/collectorModal'
-import cartCollectorButtons from './payments/cart/cartCollectorButtons'
-import cartCollectorModal from './payments/cart/cartCollectorModal'
+import { collectorEditButtons, collectorEditModal } from './SUEE'
+import { accountCollectorButtons, accountCollectorModal } from './account'
+import { cartCollectorButtons, cartCollectorModal } from './payments/cart'
 import { productCollectorButtons } from './payments/product/productCollectorButtons'
 import { productCollectorModal } from './payments/product/productCollectorModal'
 import eventCollectorButtons from './events/eventCollectorButtons'
@@ -14,8 +13,6 @@ import configCollectorButtons from './config/collectorButtons'
 import configCollectorModals from './config/collectorModals'
 import configCollectorSelect from './config/collectorSelectors'
 import { db } from '@/app'
-import accountCollectorButtons from './account/collectorButtons'
-import accountCollectorModal from './account/collectorModal'
 import { productCollectorSelect } from './payments/product/productCollectorSelect'
 
 export class ButtonController {
@@ -36,9 +33,11 @@ export class ButtonController {
 
     const productData = await db.messages.get(`${guildId}.payments.${channelId}.messages.${message?.id}`)
     if (productData !== undefined || interaction.isStringSelectMenu()) {
-      if (interaction.isButton()) { await productCollectorButtons({ interaction, key }); return }
-      if (interaction.isModalSubmit()) { await productCollectorModal({ interaction, key }); return }
-      if (interaction.isStringSelectMenu()) await productCollectorSelect({ interaction, key })
+      switch (true) {
+        case interaction.isButton(): await productCollectorButtons({ interaction, key }); break
+        case interaction.isModalSubmit(): await productCollectorModal({ interaction, key }); break
+        case interaction.isStringSelectMenu(): await productCollectorSelect({ interaction, key })
+      }
     } else {
       await interaction.reply({
         ephemeral,
@@ -54,47 +53,70 @@ export class ButtonController {
   async SUEE (): Promise<void> {
     const { interaction, key } = this
 
-    if (interaction.isButton()) { await collectorEditButtons({ interaction, key }); return }
-    if (interaction.isModalSubmit()) await collectorEditModal({ interaction, key })
+    switch (true) {
+      case interaction.isButton(): await collectorEditButtons({ interaction, key }); break
+      case interaction.isModalSubmit(): await collectorEditModal({ interaction, key }); break
+      case interaction.isStringSelectMenu():
+    }
   }
 
   async cart (): Promise<void> {
     const { interaction, key } = this
 
-    if (interaction.isButton()) { await cartCollectorButtons({ interaction, key }); return }
-    if (interaction.isModalSubmit()) await cartCollectorModal({ interaction, key })
+    switch (true) {
+      case interaction.isButton(): await cartCollectorButtons({ interaction, key }); break
+      case interaction.isModalSubmit(): await cartCollectorModal({ interaction, key }); break
+      case interaction.isStringSelectMenu():
+    }
   }
 
   async event (): Promise<void> {
     const { interaction, key } = this
 
-    if (interaction.isButton()) await eventCollectorButtons({ interaction, key })
+    switch (true) {
+      case interaction.isButton(): await eventCollectorButtons({ interaction, key }); break
+      case interaction.isModalSubmit(): break
+      case interaction.isStringSelectMenu():
+    }
   }
 
   async system (): Promise<void> {
     const { interaction, key } = this
 
-    if (interaction.isButton()) await systemCollectorButtons({ interaction, key })
+    switch (true) {
+      case interaction.isButton(): await systemCollectorButtons({ interaction, key }); break
+      case interaction.isModalSubmit():
+      case interaction.isStringSelectMenu():
+    }
   }
 
   async ticket (): Promise<void> {
     const { interaction, key } = this
 
-    if (interaction.isButton()) { await ticketCollectorButtons({ interaction, key }); return }
-    if (interaction.isModalSubmit()) { await ticketCollectorModal({ interaction, key }); return }
-    if (interaction.isStringSelectMenu()) { await ticketCollectorSelect({ interaction, key }) }
+    switch (true) {
+      case interaction.isButton(): await ticketCollectorButtons({ interaction, key }); break
+      case interaction.isModalSubmit(): await ticketCollectorModal({ interaction, key }); break
+      case interaction.isStringSelectMenu(): await ticketCollectorSelect({ interaction, key })
+    }
   }
 
   async config (): Promise<void> {
     const { interaction, key } = this
-    if (interaction.isButton()) { await configCollectorButtons({ interaction, key }); return }
-    if (interaction.isModalSubmit()) { await configCollectorModals({ interaction, key }); return }
-    if (interaction.isStringSelectMenu()) { await configCollectorSelect({ interaction, key }) }
+
+    switch (true) {
+      case interaction.isButton(): await configCollectorButtons({ interaction, key }); break
+      case interaction.isModalSubmit(): await configCollectorModals({ interaction, key }); break
+      case interaction.isStringSelectMenu(): await configCollectorSelect({ interaction, key })
+    }
   }
 
   async account (): Promise<void> {
     const { interaction, key } = this
-    if (interaction.isButton()) { await accountCollectorButtons({ interaction, key }); return }
-    if (interaction.isModalSubmit()) { await accountCollectorModal({ interaction, key }) }
+
+    switch (true) {
+      case interaction.isButton(): await accountCollectorButtons({ interaction, key }); break
+      case interaction.isModalSubmit(): await accountCollectorModal({ interaction, key }); break
+      case interaction.isStringSelectMenu():
+    }
   }
 }
