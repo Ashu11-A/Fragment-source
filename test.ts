ea2a9c3134585e01199695f56a58cf5a910eefe0
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-05-07 05:01:52 -0400

    ❌ Remove: Test

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client"
-
-const socket = io("ws://localhost:3000/")
-
-socket.on('teste', (mensagem: string) => {
-    console.log(mensagem)
-})
-
-socket.emit('Teste', 'isuhaidh')
