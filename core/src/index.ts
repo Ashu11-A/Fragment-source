d0398037e51610d9c4f6fa37dd84466aea804116
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-05-08 02:41:03 -0400

    ⌨️ Add Type

--- core/src/index.ts
+++ core/src/index.ts
@@ -7,6 +7,6 @@ const developmentEnvPath = resolve(process.cwd(), '.env.development')
 const dev = existsSync(developmentEnvPath)
 
 export const PKG_MODE = `${process.cwd()}/src` !== __dirname
-export const RootPATH = PKG_MODE ? path.join(process.cwd()) : path.join(__dirname)
+export const RootPATH: string = PKG_MODE ? path.join(process.cwd()) : path.join(__dirname, '..')
 export const metadata = async () => JSON.parse(await readFile(path.join(__dirname, '..', 'package.json'), { encoding: 'utf-8' }))
 export const { parsed: env } = dotenv.config({ path: dev ? developmentEnvPath : resolve(process.cwd(), '.env') })
