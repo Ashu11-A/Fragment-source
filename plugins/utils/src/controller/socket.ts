d1146589d8edf312c47897a749d3395e9449d216
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-05-11 22:12:21 -0400

    ✨ Better integration for send entries

--- plugins/utils/src/controller/socket.ts
+++ plugins/utils/src/controller/socket.ts
@@ -4,6 +4,7 @@ import { basename, join } from 'path'
 import { io, type Socket } from 'socket.io-client'
 import { metadata } from '..'
 import { Command, Component, Discord, Event } from '@/discord/base'
+import { formatBytes } from '../functions/format'
 
 export class SocketClient {
   public static key: string
@@ -13,39 +14,36 @@ export class SocketClient {
   connect (port: string) {
     console.log(`📡 Esperando conexão na porta ${port}...`)
     const socket = io(`ws://localhost:${port}/`)
+    const commands = Command.all.map((command) => ({ 
+      ...command,
+      defaultMemberPermissions: undefined
+     }))
     socket.on('connect', async () => {
       SocketClient.client = socket
       const files = await glob([`${join(__dirname, '..', 'entity')}/**/*.{ts,js}`])
-      let sent = 0
 
-      if (files.length === 0) {
-        const commands = Command.all.map((command) => ({ 
-          ...command,
-          defaultMemberPermissions: undefined
-         }))
+      if (files.length > 0) {
+        for (const file of files) {
+          const fileName = basename(file)
+          const code = await readFile(file)
 
+          
+          SocketClient.client.emit('entries', { total: files.length, fileName, code: code.toString('utf-8') })
+          await new Promise((resolve) => {
+            SocketClient.client.once(`${fileName}_OK`, async () => {
+              console.log(`Enviado: ${fileName} (${formatBytes(code.byteLength)})`)
+              resolve(null)
+            })
+          })
+        }
         socket.emit('info', {
           metadata: await metadata(),
           commands,
           components: Component.all,
           events: Event.all,
         })
-      } else {
-        for (const file of (files)) {
-          const fileName = basename(file)
-          sent++
-          SocketClient.client.emit('entries', { total: files.length, sent, fileName, code: await readFile(file, { encoding: 'utf-8' }) })
-        }
       }
 
-    })
-    socket.on('ready', async () => {
-      socket.emit('info', {
-        metadata: await metadata(),
-        commands: Command.all,
-        components: Component.all,
-        events: Event.all,
-      })
     })
     socket.on('kill', () => {
       process.kill(process.pid)
