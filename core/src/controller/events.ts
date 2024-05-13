ca43bcf22e4d2d5aeac50f18166de7bef5ee11f1
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-05-13 19:09:07 -0400

    🐛Fix: console retun [Object, Object}

--- core/src/controller/events.ts
+++ core/src/controller/events.ts
@@ -18,8 +18,12 @@ export class Event {
 
     this.client.onAny(async (eventName: string, args) => {
       if (eventName === 'console') {
-        console[args?.type as 'warn' | 'log' | 'error' | 'info' | undefined ?? 'log']
-        (`Plugin ${Plugins.running.find((plugin) => plugin.id ===  this.client.id)?.metadata?.name ??  this.client.id}: ${args?.message} ${args?.optionalParams}`)
+        const message = args?.message instanceof Object ? JSON.stringify(args?.message, null, 2) : args?.message
+        const optionalParams: any[] = (args?.optionalParams as any[]).map((param) => param instanceof Object ? JSON.stringify(param, null, 2): param)
+        const pluginName = Plugins.running.find((plugin) => plugin.id ===  this.client.id)?.metadata?.name ??  this.client.id
+        const type = args?.type as 'warn' | 'log' | 'error' | 'info' | undefined ?? 'log'
+
+        console[type](`Plugin ${pluginName}: ${message} ${optionalParams.join('\n')}`)
         return
       }
       if (eventName.split('_').includes('database')) { await database.events(this.client, eventName, args); return }
