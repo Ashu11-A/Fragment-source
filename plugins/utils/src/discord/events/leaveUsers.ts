677436d0bf631436439a3806bb2edad220b42b6f
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-05-13 17:50:03 -0400

    📅 Add: Discord Events in plugin utils

---
+++ plugins/utils/src/discord/events/leaveUsers.ts
@@ -0,0 +1,53 @@
+import { Event } from '@/discord/base'
+import { type TextChannel, EmbedBuilder } from 'discord.js'
+import { Database } from '@/controller/database'
+import Config from '@/entity/Config.entry'
+
+new Event({
+  name: 'guildMemberRemove',
+  async run (interaction) {
+    console.log(`Um usuário saiu do servidor: | ${interaction.guild.name} | ${interaction.user.username}`)
+    try {
+      const { guild, user } = interaction
+      const config = await new Database<Config>({ table: 'Config' }).findOne({ where: { guild: { id: guild.id } }, relations: { guild: true } })
+      // const enabled = await db.system.get(`${interaction.guild?.id}.status.systemWelcomer`)
+      // if (enabled !== undefined && enabled === false) return
+
+      // const channelStaffDB = await db.guilds.get(`${interaction?.guild?.id}.channel.staff_logs`) as string
+      // const sendStaffChannel = interaction?.guild.channels.cache.get(channelStaffDB) as TextChannel | undefined
+
+      //const userStaff = await db.staff.get(`${interaction.guild.id}.members.${interaction.user.id}`)
+
+      // if (userStaff !== undefined) {
+      //   console.log('membro da equipe encontado, removendo...')
+      //   const embed = new EmbedBuilder()
+      //     .setColor('Red')
+      //     .setTitle('📰 | STAFF LOG')
+      //     .setDescription(
+      //       `<@${interaction.user.id}> não integra mais a equipe.`
+      //     )
+      //     .setFooter({ text: `Equipe ${interaction.guild?.name}` })
+      //     .setTimestamp()
+
+      //   if (sendStaffChannel !== undefined) {
+      //     await sendStaffChannel.send({ embeds: [embed] })
+      //   }
+      //   await db.staff.delete(`${interaction.guild.id}.members.staff.${interaction.user.id}`)
+      // }
+
+      if (config?.logExit === undefined) return
+
+      const sendChannel = interaction.guild?.channels.cache.get(config.logExit)
+      if ((sendChannel?.isTextBased()) !== true) return
+
+      const embed = new EmbedBuilder({
+        description: `Usuário ${user.username}, saiu do servidor!`,
+        // footer: { text: `Equipe ${interaction.guild?.name}`, iconURL: (interaction.guild.iconURL({ size: 64 }) ?? undefined) }
+      }).setColor('Random')
+    
+      await sendChannel?.send({ embeds: [embed] })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+})
---
+++ plugins/utils/src/discord/events/onInteraction.ts
@@ -0,0 +1,59 @@
+import { Database } from '@/controller/database'
+import { Event } from '@/discord/base'
+import Config from '@/entity/Config.entry'
+import { type TextChannel, time, EmbedBuilder } from 'discord.js'
+
+new Event({
+  name: 'interactionCreate',
+  async run (interaction) {
+    if (!interaction.inCachedGuild()) return
+
+    if (interaction.isCommand()) {
+      try {
+        const { channel, user, commandName, createdAt, commandType, guild } = interaction
+        // const enabled = await db.system.get(`${interaction.guild?.id}.status.systemLogs`)
+        // if (enabled !== undefined && enabled === false) return
+
+        // const logsDB = await db.guilds.get(`${interaction?.guild?.id}.channel.logs`) as string
+        const config = await new Database<Config>({ table: 'Config' }).findOne({ where: { guild: { id: guild.id } }, relations: { guild: true } })
+
+        if (config?.logs === undefined) return
+
+        const sendChannel = await interaction.guild?.channels.fetch(config.logs)
+        if ((sendChannel?.isTextBased()) !== true) return
+ 
+        const emojis = ['⌨️', '👤', '✉️']
+        const text = [
+          'Executou o comando:',
+          'Usou o context de usuário:',
+          'Usou o context de mensagem:'
+        ]
+        const embed = new EmbedBuilder()
+          .setTitle(`Usuário ${user.username}`)
+          .addFields(
+            {
+              name: `**⚙️ ${text[commandType - 1]}**`,
+              value: ` \`${commandName}\` `,
+              inline: false
+            },
+            {
+              name: `**${emojis[commandType - 1]} há:**`,
+              value: `${time(createdAt, 'R')}`,
+              inline: false
+            },
+            {
+              name: '**🆔:**',
+              value: `${user.id}`,
+              inline: false
+            }
+          )
+          .setColor('White')
+
+        if (channel != null) embed.addFields({ name: '💬 No chat:', value: channel.url, inline: false })
+        await sendChannel.send({ embeds: [embed] })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+})
