8b244ad9d020cbf005ff332c094c27ab02ea4630
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-05-16 23:29:00 -0400

    😅 Removing createOrUpdate function (not working)

--- plugins/utils/src/discord/configs/guild.ts
+++ plugins/utils/src/discord/configs/guild.ts
@@ -24,7 +24,8 @@ new Config({
     {
       name: 'logs-equipe',
       description: '[ 📃 Logs ] Use para definir o canal de logs.',
-      type: ApplicationCommandOptionType.Channel
+      type: ApplicationCommandOptionType.Channel,
+      channelTypes: [ChannelType.GuildText]
     },
     {
       name: 'logs-geral',
@@ -43,89 +44,68 @@ new Config({
     const { options: dcOptions, guildId } = interaction
     if (guildId === null) return
     await interaction.deferReply({ ephemeral: true })
-    const banKick = dcOptions.getChannel('ban-kick') as TextChannel
-    const entrada = dcOptions.getChannel('entrada') as TextChannel
-    const logsEquipe = dcOptions.getChannel('logs-equipe') as TextChannel
-    const logsGeral = dcOptions.getChannel('logs-geral') as TextChannel
-    const saída = dcOptions.getChannel('saída') as TextChannel
+    eval
+    const banKick = dcOptions.getChannel('ban-kick') as TextChannel | null
+    const entrada = dcOptions.getChannel('entrada') as TextChannel | null
+    const logsEquipe = dcOptions.getChannel('logs-equipe') as TextChannel | null
+    const logsGeral = dcOptions.getChannel('logs-geral') as TextChannel | null
+    const saída = dcOptions.getChannel('saída') as TextChannel | null
 
     /* Database settings */
     const config = new Database<ConfigTable>({ table: 'Config' })
-    const find = { criteria: { guild: { id: guildId }} }
-    const options = { where : { guild: { id: guildId } } }
+    const dataDB = await config.findOne({ where : { guild: { id: guildId } }})
+    let data = dataDB ?? {}
+    const text = []
 
-    switch (true) {
-      case (banKick !== null): {
-        await config.createOrUpdate({
-          find,
-          update: { options, partialEntity: { logBanKick: banKick.id } },
-          create: { entity: { guild: { id: guildId }, logBanKick: banKick.id } }
-        }).then(async () => {
-          await interaction.editReply({
-            embeds: [new EmbedBuilder({
-              title: `${banKick.name} setado para o banimento ou a expulsão de usuários!`
-            })]
-          })
-        })
-      }
+    if (banKick !== null) {
+      data = { ...data, logBanKick: banKick.id }
+      text.push(`${banKick.name} setado para o banimento ou a expulsão de usuários!`)
+    }
 
-      case (entrada !== null): {
-        await config.createOrUpdate({
-          find,
-          update: { options, partialEntity: { logEntry: entrada.id } },
-          create: { entity: { guild: { id: guildId }, logEntry: entrada.id } }
-        }).then(async () => {
-          await interaction.editReply({
-            embeds: [new EmbedBuilder({
-              title: `${entrada.name} setado para a entrada de novos usuários!`
-            })]
-          })
-        })
-      }
+    if (entrada !== null) {
+      data = { ...data, logEntry: entrada.id }
+      text.push(`${entrada.name} setado para a entrada de novos usuários!`)
+    }
 
-      case (logsEquipe !== null): {
-        await config.createOrUpdate({
-          find,
-          update: { options, partialEntity: { logStaff: logsEquipe.id } },
-          create: { entity: { guild: { id: guildId }, logStaff: logsEquipe.id } }
-        }).then(async () => {
-          await interaction.editReply({
-            embeds: [new EmbedBuilder({
-              title: `${logsEquipe.name} setado para as logs de entrada e saída da equipe!`
-            })]
-          })
-        })
-      }
+    if (logsEquipe !== null) {
+      data = { ...data, logStaff: logsEquipe.id }
+      text.push(`${logsEquipe?.name} setado para as logs de entrada e saída da equipe!`)
+    }
 
-      case (logsGeral !== null): {
-        await config.createOrUpdate({
-          find,
-          update: { options, partialEntity: { logs: logsGeral.id } },
-          create: { entity: { guild: { id: guildId }, logs: logsGeral.id } }
-        }).then(async () => {
-          await interaction.editReply({
-            embeds: [new EmbedBuilder({
-              title: `${logsGeral.name} setado para os logs!`
-            })]
-          })
-        })
-      }
+    if (logsGeral !== null) {   
+      data = { ...data, logs: logsGeral.id }
+      text.push(`${logsGeral?.name} setado para os logs!`)
+    }
 
-      case (saída !== null): {
-        await config.createOrUpdate({
-          find,
-          update: { options, partialEntity: { logExit: saída.id } },
-          create: { entity: { guild: { id: guildId }, logExit: saída.id } }
-        }).then(async () => {
-          await interaction.editReply({
-            embeds: [new EmbedBuilder({
-              title: `${saída.name} setado para a saída de usuários!`
-            })]
-          })
-        })
-      }
+    if (saída !== null) {
+      data = { ...data, logExit: saída.id }
+      text.push(`${saída?.name} setado para a saída de usuários!`)
     }
     
-    console.log(await config.findOne({ where: { guild: { id: guildId } }, relations: { guild: true } }))
-  },
+    if (text.length === 0) {
+      await interaction.editReply({
+        embeds: [
+          new EmbedBuilder({
+            title: '⚠️ Nenhuma alteração foi realizada'
+          }).setColor('Orange')
+        ]
+      })
+      return
+    }
+
+    if (dataDB !== null) {
+      await config.save(data)
+    } else {
+      await config.create(data)
+    }
+
+    await interaction.editReply({
+      embeds: [new EmbedBuilder({
+        title: '📑 Configurações salvas com sucesso!',
+        description: text.join('\n')
+      })]
+    })
+    
+    console.log(data)
+  }
 })
