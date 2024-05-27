98853721e93606edb6a5e867654d894c9ba67fca
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-05-26 21:51:08 -0400

    📙 Add standardized responses

---
+++ plugins/utils/src/discord/base/CustomResponse.ts
@@ -0,0 +1,66 @@
+import { ButtonInteraction, CacheType, ColorResolvable, CommandInteraction, EmbedBuilder, ModalSubmitInteraction, StringSelectMenuInteraction } from "discord.js"
+
+
+interface ErrorOptions {
+    interaction: CommandInteraction<CacheType> | ModalSubmitInteraction<CacheType> | ButtonInteraction<CacheType> | StringSelectMenuInteraction<CacheType>
+    ephemeral?: boolean
+    element: string
+    color?: ColorResolvable
+}
+
+type TypesNotFound = 'Database' | 'Channel' | 'Message'
+type TypesInvalidProperty = 'Format'
+
+export class Error {
+  private readonly options: ErrorOptions
+  private embed: EmbedBuilder | undefined
+
+  constructor (options: ErrorOptions) {
+    this.options = options
+  }
+
+  notFound ({ type }: { type: TypesNotFound }) {
+    const { element, color } = this.options
+    let embed: EmbedBuilder
+    switch (type) {
+    case "Database": {
+      embed = new EmbedBuilder({ title: `Não encontrei ${element} no database!`})
+      break
+    }
+    case "Channel": {
+      embed = new EmbedBuilder({ title: `Não encontrei o channel ${element} no servidor!`})
+      break
+    }
+    case "Message": {
+      embed = new EmbedBuilder({ title: `Não encontrei a message ${element} no servidor!`})
+      break
+    }
+    }
+
+    embed.setColor(color ?? 'Red')
+    this.embed = embed
+    return this
+  }
+
+  invalidProperty (type: TypesInvalidProperty) {
+    const { element, color } = this.options
+    const embed = new EmbedBuilder({
+      title: `Propriedade ${element} é invalida!`
+    }).setColor(color ?? 'Red')
+    this.embed = embed
+    return this
+  }
+
+  async reply () {
+    const { interaction, ephemeral } = this.options
+
+    if (this.embed === undefined) return
+    if (interaction.isRepliable() && !interaction.replied) {
+      if (interaction.deferred) {
+        await interaction.editReply({ embeds: [this.embed] })
+        return
+      }
+      await interaction.reply({ embeds: [this.embed], ephemeral })
+    }
+  }
+}
