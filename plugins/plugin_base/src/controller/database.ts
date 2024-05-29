9a3fa42efe8869b002896ba55eacb5d4d83564cd
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-05-29 18:24:30 -0400

    ✏️ Replace once to on

--- plugins/plugin_base/src/controller/database.ts
+++ plugins/plugin_base/src/controller/database.ts
@@ -52,72 +52,72 @@ export class Database<T extends BaseEntity> {
   async save (entities: DeepPartial<T>[] | DeepPartial<T>, options?: SaveOptions): Promise<T[] | T> {
     return await new Promise((resolve, reject) => {
       SocketClient.client.emit(this.eventName, { table: this.table, plugin: this.pluginName, type: 'save', entities, options })
-      SocketClient.client.once(this.eventName, (data: T[] | T) => { resolve(data) })
-      SocketClient.client.once(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
+      SocketClient.client.on(this.eventName, (data: T[] | T) => { resolve(data) })
+      SocketClient.client.on(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
     })
   }
 
   async find (options: FindManyOptions<T> | undefined): Promise<T[]> {
     return await new Promise((resolve, reject) => {
       SocketClient.client.emit(this.eventName, { table: this.table, plugin: this.pluginName, type: 'find', options })
-      SocketClient.client.once(this.eventName, (data: T[]) => { resolve(data) })
-      SocketClient.client.once(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
+      SocketClient.client.on(this.eventName, (data: T[]) => { resolve(data) })
+      SocketClient.client.on(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
     })
   }
 
   async findBy (where: FindOptionsWhere<T>): Promise<T[]> {
     return await new Promise((resolve, reject) => {
       SocketClient.client.emit(this.eventName, { table: this.table, plugin: this.pluginName, type: 'findBy', where })
-      SocketClient.client.once(this.eventName, (data: T[]) => { resolve(data) })
-      SocketClient.client.once(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
+      SocketClient.client.on(this.eventName, (data: T[]) => { resolve(data) })
+      SocketClient.client.on(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
     })
   }
 
   async findOne (options: FindOneOptions<T>): Promise<T | null> {
     return await new Promise((resolve, reject) => {
       SocketClient.client.emit(this.eventName, { table: this.table, plugin: this.pluginName, type: 'findOne', options })
-      SocketClient.client.once(this.eventName, (data: T | null) => { resolve(data) })
-      SocketClient.client.once(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
+      SocketClient.client.on(this.eventName, (data: T | null) => { resolve(data) })
+      SocketClient.client.on(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
     })
   }
 
   async upsert (entityOrEntities: QueryDeepPartialEntity<T> | QueryDeepPartialEntity<T>[], conflictPathsOrOptions: string[] | UpsertOptions<T>): Promise<InsertResult> {
     return await new Promise((resolve, reject) => {
       SocketClient.client.emit(this.eventName, { table: this.table, plugin: this.pluginName, type: 'upsert', entityOrEntities, conflictPathsOrOptions })
-      SocketClient.client.once(this.eventName, (data: InsertResult) => { resolve(data) })
-      SocketClient.client.once(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
+      SocketClient.client.on(this.eventName, (data: InsertResult) => { resolve(data) })
+      SocketClient.client.on(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
     })
   }
 
   async create (entity: DeepPartial<T>): Promise<T> {
     return await new Promise((resolve, reject) => {
       SocketClient.client.emit(this.eventName, { table: this.table, plugin: this.pluginName, type: 'create', entity })
-      SocketClient.client.once(this.eventName, (data: T) => { resolve(data) })
-      SocketClient.client.once(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
+      SocketClient.client.on(this.eventName, (data: T) => { resolve(data) })
+      SocketClient.client.on(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
     })
   }
 
   async update (criteria: string | string[] | number | number[] | Date | Date[] | ObjectId | ObjectId[] | FindOptionsWhere<T>, partialEntity: QueryDeepPartialEntity<T>) {
     return await new Promise((resolve, reject) => {
       SocketClient.client.emit(this.eventName, { table: this.table, plugin: this.pluginName, type: 'update', criteria , partialEntity })
-      SocketClient.client.once(this.eventName, (data: DeleteResult) => { resolve(data) })
-      SocketClient.client.once(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
+      SocketClient.client.on(this.eventName, (data: DeleteResult) => { resolve(data) })
+      SocketClient.client.on(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
     })
   }
 
   async delete (criteria: string | string[] | number | number[] | Date | Date[] | ObjectId | ObjectId[] | FindOptionsWhere<T>): Promise<DeleteResult> {
     return await new Promise((resolve, reject) => {
       SocketClient.client.emit(this.eventName, { table: this.table, plugin: this.pluginName, type: 'delete', criteria })
-      SocketClient.client.once(this.eventName, (data: DeleteResult) => { resolve(data) })
-      SocketClient.client.once(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
+      SocketClient.client.on(this.eventName, (data: DeleteResult) => { resolve(data) })
+      SocketClient.client.on(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
     })
   }
 
   async count (options?: FindManyOptions<T>): Promise<number> {
     return await new Promise((resolve, reject) => {
       SocketClient.client.emit(this.eventName, { table: this.table, plugin: this.pluginName, type: 'count', options })
-      SocketClient.client.once(this.eventName, (data: number) => { resolve(data) })
-      SocketClient.client.once(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
+      SocketClient.client.on(this.eventName, (data: number) => { resolve(data) })
+      SocketClient.client.on(`${this.eventName}_error`, (data: any) => { reject(typeof data === 'object' ? JSON.stringify(data, null, 2) : data) })
     })
   }
 }
