526cba32d1c17b1c5c6f1e516ddaebeee9b3bd03
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-06-13 19:00:32 -0400

    🗑️ Delete the voice channel on last disconnection

---
+++ plugins/tickets/src/discord/events/leaveVoiceChannel.ts
@@ -0,0 +1,23 @@
+import { guildDB } from "@/functions/database.js"
+import { TextChannel } from "discord.js"
+import { Event } from "../base/Event.js"
+
+new Event({
+  name: 'voiceStateUpdate',
+  async run (oldState, newState) {
+    if (oldState.channel?.members.size === 0) {
+      const guildData = await guildDB.find({ where: { guildId: newState.guild.id }, relations: { tickets: true } })
+      const ticket = guildData[0].tickets.find((ticket) => ticket.voice?.id === oldState?.channelId)
+
+      if (ticket === undefined) return
+      if (ticket.voice?.messageId !== undefined) {
+        const channel = await newState.guild.channels.fetch(ticket.channelId).catch(() => undefined) as TextChannel | undefined
+        if (channel === undefined) return 
+
+        const message = await channel.messages.fetch(ticket.voice.messageId)
+        await message.delete()
+      }
+      await oldState.channel?.delete()
+    }
+  }
+})
