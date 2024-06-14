c4dee5575cdd2f4f505b887138f35aa811337601
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-06-13 23:56:21 -0400

    ➕ Create the config in the database too

--- plugins/tickets/src/discord/events/joinGuild.ts
+++ plugins/tickets/src/discord/events/joinGuild.ts
@@ -1,18 +1,19 @@
-import { Database } from "@/controller/database.js"
 import { Event } from "@/discord/base/index.js"
 import Guild from "@/entity/Guild.entry.js"
+import { configDB, guildDB } from "@/functions/database.js"
 
-const guildClass = new Database<Guild>({ table: 'Guild' })
-
+/**
+ * Crie o registro no banco de dados caso ele seja associado a um guild novo
+ */
 new Event({
   name: 'guildCreate',
   async run(guild) {
 
-    if (await guildClass.findOne({ where: { guildId: guild.id } }) !== null) {
+    if (await guildDB.findOne({ where: { guildId: guild.id } }) !== null) {
       console.log(`Servidor ${guild.name} está registrado no banco de dados!`)
       return
     }
-        
-    await guildClass.save(await guildClass.create({ guildId: guild.id }))
+    const result = await guildDB.save(await guildDB.create({ guildId: guild.id })) as Guild
+    await configDB.save(await configDB.create({ guild: result }))
   },
 })
