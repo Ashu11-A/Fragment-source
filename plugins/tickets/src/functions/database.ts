5d5abc00234bca1e6f15830c8e9b4ad98a75bf41
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-06-14 00:42:05 -0400

    ➕ Add interaction templateDB

--- plugins/tickets/src/functions/database.ts
+++ plugins/tickets/src/functions/database.ts
@@ -2,9 +2,11 @@ import { Database } from "@/controller/database.js";
 import Claim from "@/entity/Claim.entry.js";
 import Config from "@/entity/Config.entry.js";
 import Guild from "@/entity/Guild.entry.js";
+import Template from "@/entity/Template.entry.js";
 import Ticket from "@/entity/Ticket.entry.js";
 
 export const guildDB = new Database<Guild>({ table: 'Guild' })
 export const ticketDB = new Database<Ticket>({ table: 'Ticket' })
 export const claimDB = new Database<Claim>({ table: 'Claim' })
 export const configDB = new Database<Config>({ table: 'Config' })
+export const templateDB = new Database<Template>({ table: 'Template' })
