153bb100fad6bd2fbb97cd23e217bbb9308f2dca
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-06-13 22:07:06 -0400

    🧩 Add function for Delete in Caim

---
+++ plugins/tickets/src/discord/components/Claim/ButtonDel.ts
@@ -0,0 +1,23 @@
+import { TicketBuilder } from "@/class/TicketBuilder.js";
+import { Component } from "@/discord/base/Components.js";
+import { YouSure } from "@/discord/base/CustomIntetaction.js";
+import { Error } from "@/discord/base/CustomResponse.js";
+import { claimDB } from "@/functions/database.js";
+import { EmbedBuilder } from "discord.js";
+
+new Component({
+  customId: 'Delete',
+  type: 'Button',
+  async run(interaction) {
+    if (!interaction.inCachedGuild()) return
+    await interaction.deferReply({ ephemeral: true })
+    const { message } = interaction
+    const claimData = await claimDB.findOne({ where: { messageId: message.id }, relations: { ticket: true } })
+    if (claimData === null) throw await new Error({ element: 'este claim', interaction }).notFound({ type: "Database" }).reply()
+
+    const builder = new TicketBuilder({ interaction })
+    const isDeletable = await new YouSure({ interaction, title: 'Tem certeza que desseja deletar este ticket?' }).question()
+
+    if (isDeletable) await builder.setTicket(claimData.ticket.channelId).delete()
+  },
+})
